31f148a3a7d5e0268c2439a8c9f0aa1320daf840
Jesper <ex6tenze@pop-os.localdomain>
2022-12-14 09:07:59 +0100

    Initial commit

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import { signUp } from '../controllers/userController.js';
+
 dotenv.config();
 
 const router = express.Router();
+router.use(bodyParser.json())
 
-import { getUser, getUsers, addNewUser, removeUser, editUser, validateUser } from "../controllers/userController.js"
 
 /**
  * all appointments routes
@@ -15,23 +18,14 @@ import { getUser, getUsers, addNewUser, removeUser, editUser, validateUser } fro
       allow: 'GET, POST, OPTIONS',
       'Content-type': 'application/json',
       Data: Date.now(),
-      'Content-length': 0,
     });
     //response
     res.sendStatus(200);
   });
 
-  
-router.get('/', getUsers);
 
-router.get('/user/:id', getUser);
-
-router.post('/register', addNewUser);
-
-router.get('/login', validateUser)
-
-router.put('/user/:id', editUser)
-
-router.delete('/user/:id', removeUser);
+router.post('/register', async(req, res) => {
+  await signUp(req, res);
+});
 
 export default router
