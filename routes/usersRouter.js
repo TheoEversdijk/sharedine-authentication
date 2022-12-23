b2077bdb5cf401595b18108087fee3443eee9126
Jesper Bertijn <bert0018@hz.nl>
2022-12-23 12:26:36 +0100

    Added username/pfp verification

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import { logIn, signUp, getUsers, getSpecificUser, uploadImage } from '../controllers/userController.js';
+import { logIn, signUp, getSpecificUser, uploadImage, checkValidated, verify } from '../controllers/userController.js';
 
 dotenv.config();
 
@@ -14,14 +14,22 @@ router.post('/register', jsonParser, async(req, res) => {
 
 router.post('/login', jsonParser, async(req, res) => {
   await logIn(req, res);
-})
+});
 
 router.get('/', jsonParser, async(req, res) => {
   await getSpecificUser(req, res);
-})
+});
 
 router.post('/changeImg', jsonParser, async(req, res) => {
   await uploadImage(req, res);
-})
+});
 
-export default router
+router.post('/checkValidated', jsonParser, async(req, res) => {
+  await checkValidated(req, res);
+});
+
+router.post('/verify', jsonParser, async(req, res) => {
+  await verify(req, res);
+});
+
+export default router;
